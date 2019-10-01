1e4474857ac4d375e6aa98bc9428d1424092c162
Joseph Klutenkamper <jlkhg6@mst.edu>
2019-10-01 15:05:59 -0500

    Device can take and interpret JSON files

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+// Import the messaging module
+import * as messaging from "messaging";
+import document from "document";
+
+let marquee = document.getElementById("marquee");
+marquee.text = "yo";
+// Listen for the onmessage event
+messaging.peerSocket.onmessage = function(evt) {
+  // Output the message to the console
+  console.log(JSON.stringify(evt.data));
+  marquee.text = evt.data;
+};
+
+setTimeout(function() {
+  marquee.state = "enabled";
+}, 2000);
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,45 @@
-console.log('Hello world!');
+// Import the messaging module
+import * as messaging from "messaging";
+
+// Listen for the onopen event
+messaging.peerSocket.onopen = function() {
+  // Ready to send or receive messages
+  fetch("https://medappweb.azurewebsites.net/Person/probe")
+    .then(resp => resp.json())
+    .then(function(data) {
+      console.log(data[0].FirstName);
+      sendMessage(data[0].FirstName);
+    });
+};
+
+function sendMessage(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send the data to peer as a message
+    messaging.peerSocket.send(data);
+  }
+}
+
+// fetch("https://medappweb.azurewebsites.net/Person/probe")
+//   .then(resp => resp.json())
+//   .then(function(data) {
+//     console.log(data[0].FirstNam);
+//     sendMessage
+//   });
+
+// xmlhttp.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//     var myArr = JSON.parse(this.responseText);
+//     myFunction(myArr);
+//   }
+// };
+// xmlhttp.open("GET", url, true);
+// xmlhttp.send();
+
+// function myFunction(arr) {
+//   var out = "";
+//   var i;
+//   for(i = 0; i < arr.length; i++) {
+//     out += arr[i].display;
+//   }
+//   console.log(out);
+//}
